d9dc591fa886a52e191e1dba86fd53269b344d91
My <keshavb266@gmail.com>
2025-08-21 09:51:25 +0530

    new commit

--- components/ui/badge-button.tsx
+++ components/ui/badge-button.tsx
@@ -3,13 +3,17 @@ import { SparklesIcon } from "lucide-react"
 
 import { Badge } from "@/components/ui/badge"
 
-const BadgeButton = ({children}) => {
+type BadgeButtonProps = {
+  children: React.ReactNode
+}
+
+const BadgeButton = ({ children }: BadgeButtonProps) => {
   return (
     <Badge
       variant="outline"
       className="mb-3 cursor-pointer rounded-[14px] border border-black/10 bg-white text-base md:left-6"
     >
-      <SparklesIcon className=" mr-2  fill-[#EEBDE0] stroke-1 text-neutral-800" />{" "}
+      <SparklesIcon className="mr-2 fill-[#EEBDE0] stroke-1 text-neutral-800" />
       {children}
     </Badge>
   )
